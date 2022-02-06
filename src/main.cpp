04e43b954af4a39f16f52d49dad7fb5967318ea1
ddriver3487 <thexavierwatson@gmail.com>
2022-02-05 20:19:46 -0500

    Variant type printer implemented and functional.

--- src/main.cpp
+++ src/main.cpp
@@ -39,16 +39,16 @@ typedef boost::make_recursive_variant<
         double,
         std::string,
         std::vector<boost::recursive_variant_>,
-        std::map<std::string, boost::recursive_variant_>>::type Variant_Data;
+        std::map<std::string, boost::recursive_variant_>>::type Variant_Type;
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 struct Type_Handler : public rj::BaseReaderHandler<rj::UTF8<>, Type_Handler> {
 private:
-    Variant_Data _props;
+    Variant_Type _props;
 public:
     // && is normally only used to declare a parameter of a function. And it only takes an r-value expression.
-    Variant_Data&& Get_Value() {
+    Variant_Type&& Get_Value() {
         return std::move(_props);
     }
 
@@ -75,52 +75,10 @@ public:
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-class Get_String_Visitor
-    : public boost::static_visitor<std::string>
-{
-public:
-    std::string operator() (rj::Value& value) {
-        std::ostringstream s;
-        switch (value.GetType()) {
-            case rj::kNullType :
-                s << "Null";
-                break;
-            case rj::kFalseType :
-            case rj::kTrueType :
-                s << std::boolalpha << value.GetBool();
-                break;
-            case rj::kNumberType :
-                if (value.IsUint64()) {
-                    s << value.GetUint64();
-                } 
-                else if (value.IsInt64()) {
-                    s << value.GetInt64();
-                } 
-                else if (value.IsDouble()) {
-                    s << value.GetDouble();
-                }
-                break;
-            case rj::kStringType :
-                s << value.GetString();
-                break;
-            case rj::kObjectType :
-            case rj::kArrayType :
-                break;
-        }
-        std::string ss {s.str()};
-        boost::trim(ss);
-        return ss;
-    }
-};
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-
 //Value_Handler is a recursive function. Checks all values including child objects and arrays.
-Variant_Data Value_Handler(rj::Value& value) {
-    Get_String_Visitor print_String;
-
+Variant_Type Value_Handler(rj::Value& value) {
     if (value.IsObject()) {
-        std::map<std::string, Variant_Data> map;
+        std::map<std::string, Variant_Type> map;
         for (auto& [obj_key, obj_val] : value.GetObject()) {
             map.emplace(obj_key.GetString(), Value_Handler(obj_val));
 
@@ -129,7 +87,7 @@ Variant_Data Value_Handler(rj::Value& value) {
     }
     else if (value.IsArray()) {
         // To remove comma from printing after last value in array, subtract 1.
-        std::vector<Variant_Data> vec;
+        std::vector<Variant_Type> vec;
         for (auto& arr_value: value.GetArray()) {
             vec.push_back(Value_Handler(arr_value));
         }
@@ -144,9 +102,8 @@ Variant_Data Value_Handler(rj::Value& value) {
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-std::map<std::string ,Variant_Data> Process_Document (rj::Document& document) {
-    std::map<std::string ,Variant_Data> meta_Data;
-    Get_String_Visitor print_String;
+std::map<std::string ,Variant_Type> Process_Document (rj::Document& document) {
+    std::map<std::string ,Variant_Type> meta_Data;
 
     if (document.HasParseError()) {
         fprintf(stderr, "Error (offset %u): %s",
@@ -158,14 +115,14 @@ std::map<std::string ,Variant_Data> Process_Document (rj::Document& document) {
     if (document.IsArray()) {
         for (auto& prop : document.GetArray()) {
             if (prop.IsObject()) {
-                for (auto& [key, val]: prop.GetObject()) {
-                    meta_Data.emplace(key.GetString(), Value_Handler(val));
+                for (auto& [key, value]: prop.GetObject()) {
+                    meta_Data.emplace(key.GetString(), Value_Handler(value));
                 }
             }
         }
     } else if (document.IsObject()) {
-        for (auto& [key, val] : document.GetObject()) {
-            meta_Data.emplace(key.GetString(), Value_Handler(val));
+        for (auto& [key, value] : document.GetObject()) {
+            meta_Data.emplace(key.GetString(), Value_Handler(value));
         }
     }
     return meta_Data;
@@ -173,8 +130,8 @@ std::map<std::string ,Variant_Data> Process_Document (rj::Document& document) {
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-std::map<std::string, Variant_Data> Json_Handler() {
-    std::map<std::string, Variant_Data> map {};
+std::map<std::string, Variant_Type> Json_Handler() {
+    std::map<std::string, Variant_Type> map {};
 
         rj::Document document;
         const char* json = "[\n{\"author\": \"Xavier\","
@@ -184,14 +141,15 @@ std::map<std::string, Variant_Data> Json_Handler() {
                            "\"hello\": \"world\","
                            "\"true\": true,"
                            "\"false\": false,"
-                           "\"null\": null,"
                            "\"int64\": 123,"
                            "\"uint64\": 123456,"
                            "\"pi\": 3.1416,"
                            "\"array\": [1, 2, 3, 4],"
                            "\"nested::array\": [\"This\", \"is\", \"level\", 1, \".\","
-                           "[\"This\", \"is\", \"level\", 2, \".\"]],"
-                           "\"nested::object\":{\"level2\": {\"level3\": \"sub-value\"}}}\n]";
+                           "[\"This\", \"is\", \"level\", 2, \".\"],"
+                           "[\"This\", \"is\", \"level\", 3, \".\"]],"
+                           "\"nested::object\":{\"level2\": {\"level3\": \"sub-value\"}},"
+                           "\"array::object\":[{\"level2\": {\"level3\": [1,2,3,4]}}, [1,2,3,4]]}\n]";
         document.Parse(json);
         map = Process_Document(document);
 
@@ -199,41 +157,41 @@ std::map<std::string, Variant_Data> Json_Handler() {
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
-
-struct Recursive_Printer {
+struct Recursive_Print_T {
     using result_type = void;
     std::ostream& _os;
 
     // forwards for `operator()`
-    template <typename Ta> void call(Ta const& value) const {
-        return operator()(value);
-    }
-    // dispatch for variants
-    template <typename Tb> void operator()(boost::variant<Tb> const& value) const {
-        return boost::apply_visitor(*this, value);
-    }
+    template <typename T> void call(T const& v) const { return operator()(v); }
 
+    // dispatch for variants
+    template <typename... Ts> void operator()(boost::variant<Ts...> const& v) const {
+        return boost::apply_visitor(*this, v);
+    }
+    void operator()(nullptr_t) const {_os << "Null"; }
+    void operator()(bool b) const { _os << std::boolalpha << b; }
     void operator()(int64_t i64) const { _os << i64; }
     void operator()(u_int64_t u64) const { _os << u64; }
-    void operator()(double iD) const { _os << iD; }
-    void operator()(std::string const& string) const { _os << std::quoted(string); }
-    void operator()(bool _bool) const { _os << std::boolalpha << _bool; }
+    void operator()(double d) const { _os << d; }
+    void operator()(int i) const { _os << i; }
+    void operator()(std::string const &s) const { _os << std::quoted(s); }
 
-    template <typename Tb> void operator()(std::vector<Tb> const& array) const {
+    template <typename... Ts> void operator()(std::vector<Ts...> const& v) const {
         _os << "[ ";
         bool first = true;
-        for (auto &entry: array) {
+        for (auto& el : v) {
             if (first) {
                 first = false;
-            } else {
-                _os << ", ";
-                call(entry);
             }
-            _os << " ]";
+            else{
+                _os << ", ";
+            }
+            call(el);
         }
+        _os << " ]";
     }
 
-    template <typename Tb> void operator()(std::map<std::string, Tb> const& map) const {
+    template <typename T> void operator()(std::map<std::string, T> const& map) const {
         _os << "{ ";
         bool first = true;
         for (auto& [key, value]: map) {
@@ -241,12 +199,12 @@ struct Recursive_Printer {
                 first = false;
             } else {
                 _os << ", ";
-                call(key);
-                _os << ": ";
-                call(value);
             }
-            _os << " }";
+            call(key);
+            _os << ": ";
+            call(value);
         }
+        _os << " }";
     }
 };
 
@@ -254,13 +212,15 @@ struct Recursive_Printer {
 
 int main() {
         //Creates vector of metadata key: value maps.
-        Get_String_Visitor print_Value;
-        std::vector<std::map<std::string, Variant_Data>> meta_Data {};
+        std::vector<std::map<std::string, Variant_Type>> meta_Data {};
         meta_Data.emplace_back(Json_Handler());
+        Recursive_Print_T print {std::cout};
 
         for (auto& map : meta_Data) {
             for(auto& [key, value]: map) {
-
+                std::cout << key << ": ";
+                print(value);
+                std::cout << std::endl;
             }
         }
 
